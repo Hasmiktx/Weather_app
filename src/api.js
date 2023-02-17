13caf5a7fa11fa16fe3c8b01b1ac2f4b4903e38f
Hasmiktx <hasmiktx@gmail.com>
2023-02-17 17:13:20 +0400

    added APP

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "25a6cec173msh44de8553943b999p192cd8jsn1b63446e154c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e3160ad9b21e8220989b2bbbd1eb8c5f";
